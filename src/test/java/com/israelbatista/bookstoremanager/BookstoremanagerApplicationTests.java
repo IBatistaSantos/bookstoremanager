eb9bdfc84487891eaa0ee5e11237c79ec3873799
Israel Batista <israelbatiista19@gmail.com>
2021-01-19 21:15:17 -0300

    First commit

---
+++ src/test/java/com/israelbatista/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelbatista.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
