f640ed708015eceb1d4e6d563d332bd5c072e371
Israel Batista <israelbatiista19@gmail.com>
2021-01-21 20:22:32 -0300

    Created model mappings

---
+++ src/main/java/com/israelbatista/bookstoremanager/books/entity/Book.java
@@ -0,0 +1,42 @@
+package com.israelbatista.bookstoremanager.books.entity;
+
+import com.israelbatista.bookstoremanager.author.entity.Author;
+import com.israelbatista.bookstoremanager.publishers.entity.Publisher;
+import com.israelbatista.bookstoremanager.users.entity.User;
+import com.israelbatista.bookstoremanager.users.enums.Gender;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+
+    @Column(nullable = false)
+    private int isbn;
+
+    @Column(columnDefinition = "integer default 0")
+    private int pages;
+
+    @Column(columnDefinition = "integer default 0")
+    private int chapters;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Author author;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Publisher publisher;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private User user;
+}
