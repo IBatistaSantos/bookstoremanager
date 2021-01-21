b8228fa014bca5e6ae6b312dc8cefcfb73e1b031
Israel Batista <israelbatiista19@gmail.com>
2021-01-21 14:26:18 -0300

    Change config swagger

--- src/main/java/com/israelbatista/bookstoremanager/config/SwaggerConfig.java
+++ src/main/java/com/israelbatista/bookstoremanager/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.israelbatista.bookstoremanager.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
