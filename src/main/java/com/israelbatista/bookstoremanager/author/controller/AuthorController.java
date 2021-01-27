8ff7f49eddbab9c9cc9c92a1959e3fb421aea984
Israel Batista <israelbatiista19@gmail.com>
2021-01-27 14:13:08 -0300

    Create base class module authors our project

---
+++ src/main/java/com/israelbatista/bookstoremanager/author/controller/AuthorController.java
@@ -0,0 +1,8 @@
+package com.israelbatista.bookstoremanager.author.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+}
---
+++ src/main/java/com/israelbatista/bookstoremanager/author/service/AuthorService.java
@@ -0,0 +1,22 @@
+package com.israelbatista.bookstoremanager.author.service;
+
+
+import com.israelbatista.bookstoremanager.author.mapper.AuthorMapper;
+import com.israelbatista.bookstoremanager.author.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+    private final static AuthorMapper authorMapper = AuthorMapper.INSTANCE;
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    
+
+}
